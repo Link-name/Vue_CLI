29271342f809e610e46bac16e7977d6dce717c42
Link <stereo_house01@mail.ru>
2023-10-27 18:31:12 +0300

    dz 5 componets

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+ 
+<homepage/>
+<first_page/>
+<blook/>
+<hr>
+<productDetails/>
 </template>
+
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import homepage from './components/homepage.vue'
+import first_page from './components/first_page.vue'
+import Blook from './components/blook.vue'
+import productDetails from './components/productDetails.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+    homepage,
+    first_page,
+    Blook,
+    productDetails
   }
+
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
